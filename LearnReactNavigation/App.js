b459be5d26506c6dabb42cf0193266e65934be17
RieL <choyj1222@gmail.com>
2021-12-18 02:31:25 +0900

    5장 진행중

---
+++ LearnReactNavigation/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import DetailScreen from './screens/DetailScreen';
+import HomeScreen from './screens/HomeScreen';
+
+export default function App (){
+
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ LearnReactNavigation/screens/HomeScreen.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Button} from 'react-native';
+
+export default function HomeScreen({navigation}){
+    return (
+        <View>
+            <Button title='Detail 열기' />
+        </View>
+    );
+}
