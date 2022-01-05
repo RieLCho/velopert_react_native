75a2ec7480b462db57a844b9b41db2e3dfc7ebac
Yangjin Cho <7033942cyj@dongguk.edu>
2022-01-05 21:13:38 +0900

    6.1 완료

---
+++ DayLog/screens/SearchScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+const styles = StyleSheet.create({
+  block: {},
+});
+
+export default function SearchScreen() {
+  return <View style={styles.block} />;
+}
