def19eaf13f3e514291bc24922d3bea3597ea25b
RieL <choyj1222@gmail.com>
2022-01-15 11:59:55 +0900

    DayLog init

---
+++ Daylog/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+
+function App() {
+  return (
+    <NavigationContainer>
+      <></>
+    </NavigationContainer>
+  );
+}
+
+export default App;
