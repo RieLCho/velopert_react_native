c7370b72168cb50fbbb53b2a6255cab6e0fbc96d
RieL <choyj1222@gmail.com>
2022-01-30 22:22:20 +0900

    6.1.2

--- Daylog/App.tsx
+++ Daylog/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import RootStack from './screens/RootStack';
 
 function App() {
   return (
     <NavigationContainer>
-      <RootStack />
+      <></>
     </NavigationContainer>
   );
 }
